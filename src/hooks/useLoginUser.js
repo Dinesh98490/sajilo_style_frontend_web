c40678bbb539a394acfc27c6991803139bc5ec6c
“Dinesh98490” <kunwardinesh028@gmail.com>
2025-06-12 22:25:53 +0545

    login logics

--- src/hooks/useLoginUser.js
+++ src/hooks/useLoginUser.js
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 
 export const useLoginUser = () => {
     const navigate=useNavigate()
+    // logics of the login user 
     const {login} = useContext(AuthContext)
     return useMutation(
         {
